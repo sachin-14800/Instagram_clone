b2f7bf0281504664ccfbae029db58e0a3b6d6dd4
Sachin Gupta <sachin.14800@gmail.com>
2021-05-28 21:56:18 +0530

    acess to assests folder is set

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const port=8000;
 const express=require('express');
 
+
 const app=express();
 app.use(express.urlencoded());
 app.set('view engine','ejs');
 app.set('views','./views');
-
+app.use(express.static('assests'));
+app.get('/',function(req,res){
+    return res.render('home');
+});
 app.listen(port,function(err){
     if(err)
     {
