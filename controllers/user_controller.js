faa2a9273018b587a58311ccbff63e795ee56152
Sachin Gupta <sachin.14800@gmail.com>
2021-05-28 22:55:55 +0530

    express-ejs-layout is used to render pages

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,6 +1,7 @@
 module.exports.profile=function(req,res)
 {
     return res.render('profile',{
+        title:"Instagram",
         name:"Sachin Gupta"
     });
 }
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const port=8000;
 const express=require('express');
+const expressEjsLayouts = require('express-ejs-layouts');
 
 const db=require('./config/mongoose');
 const app=express();
 app.use(express.urlencoded());
+app.use(expressEjsLayouts);
+app.set('LayoutextractStyles',true);
+app.set('LayoutextractScripts',true);
 app.set('view engine','ejs');
 app.set('views','./views');
 app.use(express.static('assests'));
 app.use('/',require('./routes'));
-// app.get('/',function(req,res){
-//     return res.render('home');
-// });
+
 app.listen(port,function(err){
     if(err)
     {
