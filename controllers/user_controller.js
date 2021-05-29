234ffabdd35414d6e286fe5982231f3b23be3f2d
Sachin Gupta <sachin.14800@gmail.com>
2021-05-29 19:09:12 +0530

    logout functinality added

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -32,9 +32,39 @@ module.exports.signIn=function(req,res)
 
 module.exports.create=function(req,res)
 {
-    return res.redirect('/');
+    if(req.body.password!=req.body.confirm_password)
+    {
+        return res.redirect('back');
+    }
+    User.findOne({email:req.body.email},function(err,user){
+        if(err)
+        return ;
+        if(!user)
+        {
+            User.create(req.body,function(err,user){
+                if(err)
+                return;
+                return res.redirect('/user/sign-in');
+            });
+        }
+        else
+        {
+            return res.redirect('/user/sign-in');
+        }
+    });
 }
 module.exports.createSession=function(req,res)
 {
     return res.redirect('/');
+}
+
+module.exports.destroySession=function(req,res)
+{
+    // req.logout();
+    // return res.redirect('/');
+    req.session.destroy(function(err){
+        if(err)
+        return;
+        res.redirect('/');
+    })
 }
