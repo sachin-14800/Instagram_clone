639b6f341200f3fa7680e60ec49b3cea13f638cf
Sachin Gupta <sachin.14800@gmail.com>
2021-05-29 23:36:36 +0530

    delete a comment of any user on my post or my comments is done

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -15,3 +15,31 @@ module.exports.create=function(req,res){
 
     });
 }
+module.exports.destroy=function(req,res)
+{
+    
+    Comment.findById(req.params.id,function(err,comment){
+        if(err)
+        {
+            console.log(err);
+            return;
+        }
+        let postId=comment.post;
+        Post.findById(postId,function(err,post){
+            if(err)
+            return;
+            if(comment.user==req.user.id || post.user==req.user.id)
+            {  
+            comment.remove();
+            Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},function(err,post){
+                return res.redirect('back');
+            });
+            }
+            else
+            {
+                return res.redirect('back');
+            }
+        });
+       
+    });
+}
--- routes/comment.js
+++ routes/comment.js
@@ -2,4 +2,5 @@ const express=require('express');
 const router=express.Router();
 const commentController=require('../controllers/comment_controller');
 router.post('/create',commentController.create);
+router.get('/destroy/:id',commentController.destroy);
 module.exports=router;
