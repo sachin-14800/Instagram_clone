3f6f8befcce026c89753d60657e827e2015ccf89
Sachin Gupta <sachin.14800@gmail.com>
2021-06-09 00:56:11 +0530

    token created using jwt

---
+++ controllers/api/v1/user_api.js
@@ -0,0 +1,27 @@
+const User=require('../../../models/user');
+const jwt=require('jsonwebtoken');
+
+module.exports.createSession=async function(req,res)
+{
+    try{
+        let user=await User.findOne({email:req.body.email});
+        if(!user || user.password!=req.body.password)
+        {
+            return res.json(422,{
+                message:"invalid username or Password"
+            });
+        }
+        return res.json(200,{
+            message:"Sign in successful and here is your token keep it safe",
+            data:{
+                token:jwt.sign(user.toJSON(),'Instagram',{expiresIn:'10000'})
+            }
+        });
+    }catch(err)
+    {
+        return res.json(500,{
+            message:"internal Server Error"
+        });
+    }
+
+}
