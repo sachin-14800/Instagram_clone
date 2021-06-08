d7a8e6c6db9d7ab647adc1e8b037fdb9832779f0
Sachin Gupta <sachin.14800@gmail.com>
2021-06-09 02:32:42 +0530

    google authentication added

--- routes/user.js
+++ routes/user.js
@@ -18,4 +18,9 @@ router.post('/create-session',passport.authenticate(
 ),userController.createSession);
 
 router.get('/sign-out',userController.destroySession);
+
+// google authentiaction
+router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/user/sign-in'}), userController.createSession);
+
 module.exports=router;
