0333487f65a9c7f9e037ef5f487e09b826208f79
Sachin Gupta <sachin.14800@gmail.com>
2021-06-10 14:16:25 +0530

    mailer with template set up

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -2,13 +2,13 @@ const nodemailer = require('../config/nodemailer');
 const nodeMailer=require('../config/nodemailer');
 
 exports.newComment=(comment)=>{
-    console.log("inside new comment mailer",comment);
+    let htmlString=nodeMailer.renderTemplate({comment:comment},'/comments/new_comment.ejs');
     nodemailer.transporter.sendMail({
         from:'acquireurattire@gmail.com',
         // in case of post it will comment.post.user.email
         to:comment.user.email,
         subject:'New comment published !',
-        html:'<h1>Yup ,your comment is now published</h1>'
+        html:htmlString
     },(err,info)=>{
         if(err)
         {
