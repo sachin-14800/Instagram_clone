b73ab09dd78b20ddd130b7fe319bed1efcd2a3c9
Sachin Gupta <sachin.14800@gmail.com>
2021-07-01 16:47:43 +0530

    follower for not sign-in user creating home page not to load error fixed

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -17,10 +17,14 @@ module.exports.home=async function(req,res)
         }
     }).populate('likes');
     let users= await User.find({});
-    let user=await User.findById(req.user._id);
-    let chats=await Follow.find({to_user:user})
+    let chats;
+    let user;
+    if(req.user){
+     user=await User.findById(req.user._id);
+    chats=await Follow.find({to_user:user})
     .populate('from_user')
     .populate('to_user');
+    }
     return res.render('home',{
         title:"Home page",
         post:posts,
