13f828aa88dca4cefe44d91c87d6fe1dfe5a41e6
Sachin Gupta <sachin.14800@gmail.com>
2021-07-02 20:55:56 +0530

    chat socket comments added

--- assests/js/chat_engine.js
+++ assests/js/chat_engine.js
@@ -1,9 +1,11 @@
-
+//Class for starting up the chat engine
 class ChatEngine{
+    //constructor to assign required parameters
     constructor(chatBoxId, userid,recieverid){
         this.chatBox = $(`#${chatBoxId}`);
         this.userid = userid;
         this.recieverid=recieverid;
+        //creating a unique id for user to user chat experience
         let compare=userid.localeCompare(recieverid);
             let room;
             if(compare>0)
@@ -11,10 +13,11 @@ class ChatEngine{
             else
             room=recieverid+userid;
         this.chatroom=room;
+        //starting the socket
         this.socket = io.connect('http://localhost:5000',{
             withCredentials: true,
         });
-
+        // function to start all socket events    
         if (this.userid){
             this.connectionHandler();
         }
@@ -25,25 +28,27 @@ class ChatEngine{
     connectionHandler(){
         let self=this;
         this.socket.on('connect', function(){
-            // console.log('connection established using sockets...!');
+            //established a connection using sockets
             
+            // emiting an event to create room
             self.socket.emit('join_room',{
                 userid:self.userid,
                 recieverid:self.recieverid,
                 chatroom:self.chatroom
             });
+
+            //recieving an event when a user is joined
             self.socket.on('user_joined',function(data){
-                // console.log('A user is joined',data.userid);
-                // console.log('A user is joined',data.recieverid);
+                //user joined
             });
         });
 
         $('#send-message').click(function(){
             
             let msg=$('#chat-message-input').val();
+            // if message is present emit a event for sending the message 
             if(msg!="")
             {
-                // console.log(self.chatroom);
                 self.socket.emit('send_message',{
                     message:msg,
                     userid:self.userid,
@@ -53,8 +58,8 @@ class ChatEngine{
             }
         });
 
+        // recieving an event for recieving a message
         self.socket.on('receive_message',function(data){
-            // console.log('message received',data.message);
 
             let newMessage=$('<li>');
             let messageType='other-message';
@@ -62,21 +67,31 @@ class ChatEngine{
             {
                 messageType='self-message';
             }
+            // creating a new message
             newMessage.append($('<span>',{
                 'html':data.message
             }));
+            // class to determine which user message it is
             newMessage.addClass(messageType);
+
+            //prepennding a message in the chat list
             $('#chat-messages-list').prepend(newMessage);
+
+            //empty the input message that is already send
             $('#chat-message-input').val('');
+
+            //autoscroll to the bottom when a new message arrives
             scroll_to_bottom($('#chat-message-input'));
         });   
     }
 }
+
+//function to autoscroll the chat list
 var scroll_to_bottom = function(element){
+    //trying to scroll the element
     var tries = 0, old_height = new_height = element.height();
     var intervalId = setInterval(function() {
         if( old_height != new_height ){    
-            // Env loaded
             clearInterval(intervalId);
             element.animate({ scrollTop: new_height }, 'slow');
         }else if(tries >= 30){
--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -1,4 +1,10 @@
+//middleware for starting the chat engine to communicate from frontend
+
+
+//model for storing the messages of users
 const Message=require('../models/messages');
+
+
 module.exports.chatSockets = function(socketServer){
     let io = require('socket.io')(socketServer,{
         cors:{
@@ -7,24 +13,28 @@ module.exports.chatSockets = function(socketServer){
             credentials: true
         }
     });
-
+    //connecting the socket
     io.sockets.on('connection', function(socket){
         console.log('new connection received', socket.id);
 
+        //when socket is disconnected
         socket.on('disconnect',function(){
             console.log('socket disconnected!');
         });
 
+        //event recieved for joining the room
         socket.on('join_room',function(data){
-            // console.log('Joining request recieved',data);
             //if exists then enter the user else create the chatroom
             socket.join(data.chatroom);
             //emit in a specific chatroom
             io.in(data.chatroom).emit('user_joined',{userid:data.userid,recieverid:data.recieverid});
         });
 
+        //event recieved when message is send to other user
         socket.on('send_message',async function(data){
             let message=Message.create({room:data.chatroom,from_user:data.userid,to_user:data.recieverid,content:data.message});
+
+            //emiting an event for recieving the message 
             io.in(data.chatroom).emit('receive_message',data);
         });
 
