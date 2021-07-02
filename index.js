d3b02fff63640d4601cce1c3fdecd0544e2bd0ac
Sachin Gupta <sachin.14800@gmail.com>
2021-07-02 22:01:57 +0530

    comments for index file are added

--- index.js
+++ index.js
@@ -1,25 +1,60 @@
+//port number
 const port=8000;
+//module for express 
 const express=require('express');
+
 // const env=require('./config/environment');
+
+//module for ejs layout for frontend
 const expressEjsLayouts = require('express-ejs-layouts');
+
+//module for cookies of the site
 const cookieParser=require('cookie-parser');
+
+//importing the config database file
 const db=require('./config/mongoose');
+
+//creating a new express app
 const app=express();
+
+//module for session of a particular user
 const session=require('express-session');
+
+//module for autentication
 const passport=require('passport');
+
+//module for local authentication imported for config
 const passportLocal=require('./config/passport-local-strategy');
+
+//module for jwt authentication imported from config
 const passportJWT=require('./config/passport-jwt-strategy');
+
+//importing module for google authentication from config
 const passportGoogle=require('./config/passport-google-oauth-strategy');
+
+//module for storing the session 
 const MongoStore=require('connect-mongo');
+
+//sass middle for converting scss to css
 const sassMiddleware=require('node-sass-middleware');
+
+//module for flashing up of messages
 const flash=require('connect-flash');
+
+//middleware for setting the flash message
 const customMware=require('./config/middleware');
 
+//necesaary imports for chatserver(Sockets)
 const chatServer=require('http').Server(app);
 const  chatSockets=require('./config/chat_sockets').chatSockets(chatServer);
+//chat server running on port 5000
 chatServer.listen(5000);
 console.log('Chat server is running on port 5000');
+
+//path module
 const path=require('path');
+
+//telling express to use sass middleware
 app.use(sassMiddleware({
     src:'./assests/scss',
     dest:'./assests/css',
@@ -27,19 +62,26 @@ app.use(sassMiddleware({
     outputStyle:'expanded',
     prefix:'/css',
 }));
+
 app.use(express.urlencoded());
+//cookie parser
 app.use(cookieParser());
 
+//static files location
 app.use(express.static('./assests')); //path.join(__dirname,env.asset_path)
 // make upload path available to the browser
 app.use('/uploads',express.static(__dirname+'/uploads'));
+//telling app to use express layouts
 app.use(expressEjsLayouts);
+//set script and style true for ejs layouts
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
+//teling express for the different views
 app.set('view engine','ejs');
 app.set('views','./views');
 
+//initialising the express session
 app.use(session({
     name:'instagram',
     secret:'blahsomething',   //env.session_cookie_key
@@ -52,14 +94,23 @@ app.use(session({
         mongoUrl:'mongodb://localhost/instagram_clone'
     })
 }));
+
+//initialising the passport and its session
 app.use(passport.initialize());
 app.use(passport.session());
+//authenticating the user
 app.use(passport.setAuthenticatedUser);
+
+//using flash for displaying the messages
 app.use(flash());
+
+//setting flash messages using the custom midlleware
 app.use(customMware.setFlash);
 
+//adding different routes
 app.use('/',require('./routes'));
 
+//strating the express server
 app.listen(port,function(err){
     if(err)
     {
