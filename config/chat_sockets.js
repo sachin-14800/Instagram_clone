425d9925bd33de53f9666bc7c14c4e3b4fe4f354
sachin-14800 <sachin.14800@gmail.com>
2021-12-24 19:46:52 +0530

    cors issue solved

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -6,13 +6,7 @@ const Message=require('../models/messages');
 
 
 module.exports.chatSockets = function(socketServer){
-    let io = require('socket.io')(socketServer,{
-        cors:{
-            origin:'http://localhost:8000',
-            methods: ["GET", "POST"],
-            credentials: true
-        }
-    });
+    let io = require('socket.io')(socketServer);
     //connecting the socket
     io.sockets.on('connection', function(socket){
         console.log('new connection received', socket.id);
